28ec4f4c7d8985229ae0ba91daa8e6fcb89ff3ff
Tyler Rick <github.com@tylerrick.com>
2008-11-20 22:31:23 -0800

    Ran rails command with version 2.2.2 to make sure everything is up-to-date.

--- test/integration/app/app/controllers/application.rb
+++ test/integration/app/app/controllers/application.rb
@@ -2,6 +2,17 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_testapp_session_id'
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  #protect_from_forgery # :secret => 'c9c8eb8eea05c920241b41e7266e425f'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
 end
--- test/integration/app/config/environments/development.rb
+++ test/integration/app/config/environments/development.rb
@@ -1,8 +1,17 @@
+# Settings specified here will take precedence over those in config/environment.rb
 
-config.cache_classes = ENV['PRODUCTION']
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for development
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
+
+# Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
-config.action_controller.consider_all_requests_local = !ENV['PRODUCTION']
-config.action_controller.perform_caching = ENV['PRODUCTION']
-config.action_view.cache_template_extensions = ENV['PRODUCTION']
-config.action_view.debug_rjs = !ENV['PRODUCTION']
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+
+# Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
